59c27c1d530c8b501c70d4777cdeb1126fd97444
sushma <basanisushma777@gmail.com>
2022-10-12 13:18:59 +0530

    updated put method

--- first.java
+++ first.java
@@ -4,6 +4,9 @@ public class first {
 	public static void push(){
 
 	}
+	public void put(int m){
+
+	}
 static void patt(int n) {
 	
 	for(int i=1;i<=n;i++) {
@@ -22,6 +25,7 @@ static void patt(int n) {
 	public static void main(String[] args) {
 patt(4);
 push();
+put(7);
 	}
 
 }
