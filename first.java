35086d9af5759454418eb6adfc2220fe2a1fe723
sushma <basanisushma777@gmail.com>
2022-10-12 10:00:50 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,23 @@
+package javaapp;
+
+public class first {
+static void patt(int n) {
+	
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.print(i);
+		}
+		System.out.println();
+	}
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n-i+1;j++) {
+			System.out.print(i);
+		}
+		System.out.println();
+	}
+}
+	public static void main(String[] args) {
+patt(4);
+	}
+
+}
