a252d0bd53d4d99e59fbc404225d98b2bab8099d
sushma6677 <115568443+sushma6677@users.noreply.github.com>
2022-10-21 11:24:40 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,19 @@
+package assingmentpro1;
+
+public class pattern4 {
+
+	public static void main(String[] args) {
+		int n=12;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(j==0&&i>(n-1)/2||i==n-1||i-j>=(n-1)/2||i+j>=n-1+((n-1)/2)||j==n-1&&i>(n-1)/2) {
+					System.out.print("*");}
+					else {
+						System.out.print(" ");
+					}
+				
+			}System.out.println( );
+		}
+	}
+
+}
---
+++ pattern5.java
@@ -0,0 +1,19 @@
+package assingmentpro1;
+
+public class pattern5 {
+
+	public static void main(String[] args) {
+		int n=12;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(i==0||i==n-1||j==0||i+j<=(n-1)/2||i-j>=(n-1)/2) {
+					System.out.print("*");}
+					else {
+						System.out.print(" ");
+					}
+				
+			}System.out.println( );
+		}
+	}
+
+}
